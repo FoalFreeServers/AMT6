7e6d4d8d64baa095951bf580034b033fa5d02044
Sydney <github@shonx.net>
2022-08-06 23:23:16 -0400

    Sync from Server.

--- world/computercraft/computer/3/DisplayP1.lua
+++ world/computercraft/computer/3/DisplayP1.lua
@@ -1,21 +1,37 @@
-term.clear()
-term.setCursorPos(1,1)
-print("Monitoring Reactor...")
 R1 = peripheral.find("fissionReactorLogicAdapter")
 R2 = peripheral.find("fissionReactorPort")
 assert(R1,"Reactor Logic Missing")    
 assert(R2,"Reactor Port Missing")
 assert(R1.isFormed(),"Reactor Structure Incomplete")
+
 function KtC(TempinK) 
    return TempinK - 273.15
 end
 
+--function Nominality()
+--    if R1.getStatus()
+--        then RFunction = "Offline"
+--    elseif (KtC(Temp)) > 290
+--        then RFunction = "System Malfunction"
+--    elseif (CoolSod < 0.05)
+--        then RFunction = "System Malfunction"
+--    elseif (HotSod < 0.5)
+--        then RFunction = "System Malfunction"
+--    elseif (Damage > 0)
+--        then RFunction = "System Malfunction"
+--    elseif (Waste > 0.5)
+--        then RFunction = "System Malfunction"
+--    else RFunction = "All Systems Nominal"
+--    end
+   -- else RFunction = "Offline"
+   -- end
+--end
+    
 while R1.isFormed() do
-term.clear()
-term.setCursorPos(1,1)
 
+    local RFunction = "All Systems Nominal"
     local Temp = R1.getTemperature()
-    local Status = R1.getStatus
+    local Status = R1.getStatus()
     local Fuel = R1.getFuelFilledPercentage()
     local CoolSod = R1.getCoolantFilledPercentage()
     local HotSod = R1.getHeatedCoolantFilledPercentage()
@@ -24,14 +40,22 @@ term.setCursorPos(1,1)
     local Burn = R1.getBurnRate()
     local MaxBurn = R1.getMaxBurnRate()
     local ActBurn = R1.getActualBurnRate()
-      
-print("------------------------------")
---print("Status:"..S)
-print("Temperature:"..math.floor(KtC(Temp)).."C")
-print("Fuel Level:"..math.floor(Fuel*100).."%")
-print("Processing:"..ActBurn.."/"..MaxBurn.."mB/t")
-print("------------------------------")
+    
+term.clear()
+term.setCursorPos(1,1)
+print(Status)
+
+   -- if Status = "false" then RFunction = "Offline"
+   -- end
+    
+print("Monitoring Reactor..."..RFunction)     
+print("------------------------------------")
+--print("Status:"..(Status))
+print("Temperature : "..math.floor(KtC(Temp)).."C")
+print("Fuel Level  : "..math.floor(Fuel*100).."%")
+print("Processing  : "..ActBurn.."/"..MaxBurn.."mB/t")
+print("------------------------------------")
 print("Hot Coolant in Vessel:"..HotSod)
-print("Coolant Supply in Vessel:"..math.floor(CoolSod*22.5).."/22.5kB")
+print("Coolant Supply in Vessel:"..(math.floor(CoolSod*100*22.5)*0.01).."/22.5kB")
 sleep(0.25)
 end
